c8507f889c8b188d1d2bc47ada8b291c943d068e
root <root@fire-han.ga>
2017-05-14 19:00:56 +0900

    db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'han_kang' });
 });
 
+//test json
+router.get('/', function(req, res, next) {
+  fs.readFile('database/todo.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+    res.render('index', {todo:JSON.parse(data)});
+
+  });
+});
 module.exports = router;
